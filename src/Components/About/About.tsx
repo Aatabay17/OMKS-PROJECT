600617d94a382e21d6e9bf862dbb550158f8cbaf
RakhatAubakirov <rakha.aubakirov@gmail.com>
2020-10-23 02:31:18 +0600

    added changes

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -14,6 +14,7 @@ export default function About({}: Props): ReactElement {
             <span className = 'about-main-title'>About Us</span>
             <div className="about_blocks">
                 <div className="blocks">
+                    <i className="fas fa-question-circle fa-4x"></i><br></br>
                     <span className = 'about-block-title'>For What?</span>
                     <p className="about_p">
                         Do you have student stories, discussions, lost things? Or maybe invitations to events?<br></br>
@@ -21,12 +22,14 @@ export default function About({}: Props): ReactElement {
                     </p>
                 </div>
                 <div className="blocks">
+                    <i className="fas fa-users fa-4x"></i><br></br>
                     <span className = 'about-block-title'>Who We Are?</span>
                     <p className="about_p">
                         One Million KBTU Stories is a service that provides students of our university to share their stories, thoughts and suggestions with friends.
                     </p>
                 </div>
                 <div className="blocks">
+                    <i className="fas fa-bullseye fa-4x"></i><br></br>
                     <span className = 'about-block-title'>Our Mission</span>
                     <p className="about_p">
                     Our goal is to give students opportunity to has their own social media where they can read other posts, feedback them and add their own stories.
